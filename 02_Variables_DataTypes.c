2252d8c47f34b2386939a0deb12a6246bcd8da1e
Shruti suryawanshi <suryawanshishruti165@gmail.com>
2026-08-15 16:22:58 +0530

    Add basic variable declarations and print statements

---
+++ 02_Variables_DataTypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 18;
+    float marks = 85.5;
+    char grade = 'A';
+
+    printf("Age = %d\n", age);
+    printf("Marks = %.2f\n", marks);
+    printf("Grade = %c\n", grade);
+
+    return 0;
+}
