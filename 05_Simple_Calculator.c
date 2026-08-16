c7f5140c083064b60d0eaddde86e8a2264810e3f
Shruti suryawanshi <suryawanshishruti165@gmail.com>
2026-08-16 18:49:00 +0530

    Create 05_Simple_Calculator.c

---
+++ 05_Simple_Calculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    float a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    printf("Addition = %.2f\n", a + b);
+    printf("Subtraction = %.2f\n", a - b);
+    printf("Multiplication = %.2f\n", a * b);
+    printf("Division = %.2f\n", a / b);
+
+    return 0;
+}
