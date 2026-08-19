8cc73afe30476c85bbf7f13e3d2fa9a3c495a486
Shruti suryawanshi <suryawanshishruti165@gmail.com>
2026-08-19 19:58:28 +0530

    Day 3

---
+++ 08_Positive_Negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n >= 0)
+    {
+        printf("Positive Number");
+    }
+    else
+    {
+        printf("Negative Number");
+    }
+
+    return 0;
+}
